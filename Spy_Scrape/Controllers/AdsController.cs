6b16111dd662ffe9126035ff59ccfce20a0a07d3
Denis Zavertyayev <denisz2277@gmail.com>
2021-05-04 09:38:26 -0700

    Created an Ads and TrafficSources controllers.

---
+++ Spy_Scrape/Controllers/AdsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Spy_Scrape.Data;
+using Spy_Scrape.Models;
+
+namespace Spy_Scrape.Controllers
+{
+    public class AdsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ads
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Ads.ToListAsync());
+        }
+
+        // GET: Ads/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ad = await _context.Ads
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ad == null)
+            {
+                return NotFound();
+            }
+
+            return View(ad);
+        }
+
+        // GET: Ads/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ads/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AdSource,AdType,AdOs,AdTargetMarket,AdMarketCountry,TrafficSourceId")] Ad ad)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ad);
+        }
+
+        // GET: Ads/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ad = await _context.Ads.FindAsync(id);
+            if (ad == null)
+            {
+                return NotFound();
+            }
+            return View(ad);
+        }
+
+        // POST: Ads/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AdSource,AdType,AdOs,AdTargetMarket,AdMarketCountry,TrafficSourceId")] Ad ad)
+        {
+            if (id != ad.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ad);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdExists(ad.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ad);
+        }
+
+        // GET: Ads/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ad = await _context.Ads
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ad == null)
+            {
+                return NotFound();
+            }
+
+            return View(ad);
+        }
+
+        // POST: Ads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ad = await _context.Ads.FindAsync(id);
+            _context.Ads.Remove(ad);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AdExists(int id)
+        {
+            return _context.Ads.Any(e => e.Id == id);
+        }
+    }
+}
