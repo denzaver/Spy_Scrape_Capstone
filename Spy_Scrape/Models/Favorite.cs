519c5b45427402335b8f4fc6af66a82a1ee54aaf
Denis Zavertyayev <denisz2277@gmail.com>
2021-05-04 13:34:33 -0700

    Created a Favorites model, passed in a list of ads and a foreign key of customer.

---
+++ Spy_Scrape/Models/Favorite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spy_Scrape.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int Id { get; set; }
+        public List<Ad> Ads { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}
