dbb8e7a9eff4e89487862a7742fdf8638baa5f88
Denis Zavertyayev <denisz2277@gmail.com>
2021-05-06 04:48:22 -0700

    Created an AdCategory model with an Id property and a Category type.

---
+++ Spy_Scrape/Models/AdCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spy_Scrape.Models
+{
+    public class AdCategory
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Display(Name = "Ad Type")]
+        public string CategoryType { get; set; }
+    }
+}
