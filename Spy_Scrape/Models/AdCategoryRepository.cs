fd3065eb33907374c1fbeec968154f1e485f908e
Denis Zavertyayev <denisz2277@gmail.com>
2021-05-06 23:18:16 -0700

    Created an AdCategoryRepository class to implement the IAdCategoryRepository. Testing app, still working.

---
+++ Spy_Scrape/Models/AdCategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Spy_Scrape.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spy_Scrape.Models
+{
+    public class AdCategoryRepository : IAdCategoryRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public AdCategoryRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IEnumerable<AdCategory> GetAdCategories => _applicationDbContext.AdCategories;
+    }
+}
