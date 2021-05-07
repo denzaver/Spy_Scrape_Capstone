3237f0c57eb4fd55f8f45a15774a6fff2e34a576
Denis Zavertyayev <denisz2277@gmail.com>
2021-05-07 02:57:40 -0700

    Populated the AdMarketplace view model with 2 properties, IEnumerable list and a string CurrentCategory.

--- Spy_Scrape/ViewModels/AdMarketplaceViewModel.cs
+++ Spy_Scrape/ViewModels/AdMarketplaceViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spy_Scrape.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,7 @@ namespace Spy_Scrape.ViewModels
 {
     public class AdMarketplaceViewModel
     {
+        public IEnumerable<Ad> Ads { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
