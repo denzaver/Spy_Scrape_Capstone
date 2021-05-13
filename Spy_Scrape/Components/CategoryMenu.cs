d24a54f97556f32238d932ce1d92323f34f6f188
Denis Zavertyayev <denisz2277@gmail.com>
2021-05-12 18:14:49 -0700

    Pssed in the ICategory repository and established the constructor.

--- Spy_Scrape/Components/CategoryMenu.cs
+++ Spy_Scrape/Components/CategoryMenu.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Spy_Scrape.Models;
 
 namespace Spy_Scrape.Components
 {
-    public class CategoryMenu
+    public class CategoryMenu : ViewComponent
     {
+        private readonly IAdCategoryRepository _adCategoryRepository;
+
+        public CategoryMenu(IAdCategoryRepository adCategoryRepository)
+        {
+            _adCategoryRepository = adCategoryRepository;
+        }
     }
 }
